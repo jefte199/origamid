0f9a4aee73d0159b944bcfe9cebf8bd8ddceaad9
Jefte <jefte199@gmail.com>
2021-06-23 09:48:52 -0300

    feat: create webpack

---
+++ react/exercicios/webpack/src/App.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+const App = () => {
+  return react.createElement(
+    'img',
+    {src: 'https://raw.githubusercontent.com/Martim-Fuhr/JogoDaMemoria/main/img/charizard.png'}
+  )
+}
+
+export default App;
---
+++ react/exercicios/webpack/src/index.js
@@ -0,0 +1,6 @@
+import ReactDom from 'react-dom';
+import react from 'react';
+
+import app from './App';
+
+ReactDom.render(app(), document.getElementById('root'));
