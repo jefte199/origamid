61bf4829d4e52326f5aadfd6e22c2948faa05c30
Jefte <jefte199@gmail.com>
2021-09-07 08:40:32 -0300

    feat: create component

--- react/exercicios/meuapp/src/App.js
+++ react/exercicios/meuapp/src/App.js
@@ -1,47 +1,9 @@
 import React from 'react';
 
+import Form from './form/Form';
+
 const App = () => {
-
-  const produtos = [
-    {
-      id: 1,
-      nome: 'Smartphone',
-      preco: 'R$ 2000',
-      cores: ['#29d8d5', '#252a34', '#fc3766'],
-    },
-    {
-      id: 2,
-      nome: 'Notebook',
-      preco: 'R$ 3000',
-      cores: ['#ffd045', '#d4394b', '#f37c59'],
-    },
-    {
-      id: 3,
-      nome: 'Tablet',
-      preco: 'R$ 1500',
-      cores: ['#365069', '#47c1c8', '#f95786'],
-    },
-  ];
-
-  return (
-    <>
-      {
-        produtos
-          .filter(({ preco }) => parseInt(preco.split(" ")[1]) > 1500)
-          .map(({ id, nome, preco, cores }) => {
-            return (
-              <div key={id}>
-                <h1 >{nome}</h1>
-                <p>Preço: {preco}</p>
-                <ul>
-                  {cores.map((cor) => <li style={{backgroundColor: cor, color: "white"}} key={cor}>{cor}</li>)}
-                </ul>
-              </div>
-            )
-          })
-      }
-    </>
-  );
+  return <Form />;
 };
 
 
---
+++ react/exercicios/meuapp/src/form/Form.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Button from './Button';
+import Input from './Input';
+
+const Form = () => {
+  return (
+    <>
+      <p>
+        <label>Senha</label>
+        <Input />
+      </p>
+      <p>
+        <label>Senha</label>
+        <Input />
+      </p>
+      
+      <Button />
+    </>
+  )
+};
+
+export default Form;
