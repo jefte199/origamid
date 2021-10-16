e56ed8591d3329cfe25a1ae58e94cc2aa763e3a2
Jefte <jefte199@gmail.com>
2021-10-16 11:30:05 -0300

    feat: create card bootstap

--- react/exercicios/meuapp/src/App.js
+++ react/exercicios/meuapp/src/App.js
@@ -3,8 +3,14 @@ import React from 'react';
 const App = () => {
   return (
     <>
-    <div className="card bg-dark text-white m-5">
+    <div className="card bg-dark text-white m-5" style={{ maxWidth: '18rem' }}>
       <div className="card-header">Notebook</div>
+      <div className="card-body">
+        <h5 className="card-title">R$ 2500</h5>
+        <p className="card=text">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic distinctio fuga tenetur et est culpa! 
+        </p>
+      </div>
     </div>
     </>
   );
