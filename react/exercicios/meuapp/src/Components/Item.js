0ce4c429213f73d41e5364bdbe116f60c5f46bb9
Jefte <jefte199@gmail.com>
2022-01-23 21:30:50 -0300

    remove: useeffect

--- react/exercicios/meuapp/src/Components/Item.js
+++ react/exercicios/meuapp/src/Components/Item.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import Head from './Head';
-import axios from 'axios';
 import style from './Item.module.css';
 
 export default function Item() {
-  const baseURL = "https://ranekapi.origamid.dev/json/api/produto";
 
   const { item, nome, preco, descricao } = useParams();
-  const [produtos, setProdutos] = React.useState(null);
 
   if (item === null) return null;
   return (
