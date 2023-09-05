018e350577b65ab6ae4e19a98dc389bc28d7112b
Jefte <jefte199@gmail.com>
2023-09-05 10:05:14 -0300

    feat: add gulp-autoprefixed

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,10 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
 
 function compilaSass() {
   return gulp
     .src('css/scss/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
     .pipe(gulp.dest('css/'))
 }
 
