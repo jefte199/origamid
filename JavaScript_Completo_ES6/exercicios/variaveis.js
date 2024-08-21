c414535d568548da1551c10cc650f7bcf0d70ca5
Jefte <jefte199@gmail.com>
2024-08-21 08:21:05 -0300

    studing: object

--- JavaScript_Completo_ES6/exercicios/variaveis.js
+++ JavaScript_Completo_ES6/exercicios/variaveis.js
@@ -1,10 +1,31 @@
-var totalPaises = 193;
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+const pessoa = {
+  name: "Jefté",
+  lastName: "Rufino",
+  age: 27,
+  full: function () { return `I am ${this.name} ${this.lastName} and i am ${this.age} years old` },
+}
 
-function precisoVisitar(paisesVisitados) {
-  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+pessoa.name = "Edvania";
+pessoa.age = 21;
+console.log(pessoa.full());
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+
+const dog = {
+  name: "rex",
+  breed: "labrador",
+  age: 4,
+  color: "#000",
+  al(see) {
+    if (see === "homem") {
+      return "Latir"
+    } else {
+      return "dont have"
+    }
+  }
 }
-function jaVisitei(paisesVisitados) {
-  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
-}
-console.log(precisoVisitar(20));
-console.log(jaVisitei(20));
+
+console.log(`dog ${dog.name} ${dog.al('homem')}`)
