aa7b19f79821006f10719db36edffd503c56e21a
Jefte <jefte199@gmail.com>
2024-08-22 10:34:41 -0300

    studing: escopo

--- JavaScript_Completo_ES6/exercicios/variaveis.js
+++ JavaScript_Completo_ES6/exercicios/variaveis.js
@@ -1,24 +1,29 @@
-// Crie uma array com os anos que o Brasil ganhou a copa
-// 1959, 1962, 1970, 1994, 2002
-const nation = [1959, 1962, 1970, 1994, 2002]
-
-// Interaja com a array utilizando um loop, para mostrar
-// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
-//nation.forEach((item, index) => {
-//  console.log(`O Brasil ganhou a copa no ano de ${item}, essa foi a ${index + 1} vez`)
-//})
-
-// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
-var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
-for (let i = 0; i < frutas.length; i++) {
-  console.log(frutas[i]);
-  if (frutas[i] === "Uva") {
-    break;
-  }
+// Por qual motivo o código abaixo retorna com erros?
+{
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+  console.log(cor, marca, portas);
 }
 
-// Coloque a última fruta da array acima em uma variável,
-// sem remover a mesma da array.
 
-const newFruit = frutas[frutas.length - 1]
-console.log(newFruit)
+// Como corrigir o erro abaixo?
+const dois = 2;
+function somarDois(x) {
+  return x + dois;
+}
+function dividirDois(x) {
+  return x + dois;
+}
+somarDois(4);
+dividirDois(6);
+
+// O que fazer para total retornar 500?
+const numero = 50;
+
+for(let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
